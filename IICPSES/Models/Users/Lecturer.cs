71a58d25c334260244d61247ba837733e4ef1cea
Adrian Khor <adriank96@outlook.com>
2016-08-30 13:38:46 +0800

    added User, Lecturer models

---
+++ IICPSES/Models/Users/Lecturer.cs
@@ -0,0 +1,7 @@
+﻿namespace IICPSES.Models.Users
+{
+    public class Lecturer : User
+    {
+        public int LecturerID { get; set; }
+    }
+}
---
+++ IICPSES/Models/Users/User.cs
@@ -0,0 +1,9 @@
+﻿namespace IICPSES.Models.Users
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Occupation { get; set; }
+    }
+}
