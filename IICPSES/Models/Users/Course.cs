2f022438a52e6d8d53df2ee9417d9a5623d864cd
hanny562 <hanny562@hotmail.com>
2016-09-01 13:51:52 +0800

    Add Course base class

---
+++ IICPSES/Models/Users/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IICPSES.Models.Users
+{
+    public class Course
+    {
+        public int Semester { get; set; }
+        public string SemesterSession { get; set; }
+        public int SubjectName { get; set; }
+        public string ProgramName { get; set; }
+    }
+}
