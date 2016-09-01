8f8eea0480ad2173ce7df61f4542183f52ebdf34
hanny562 <hanny562@hotmail.com>
2016-09-01 13:40:47 +0800

    add student class, SubjectClass

---
+++ IICPSES/Models/Users/SubjectProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IICPSES.Models.Users
+{
+    public class SubjectProfile
+    {
+        public string SchoolName { get; set; }
+        public string ProgramName { get; set; }
+        public string SubjectName { get; set; }
+    }
+}
